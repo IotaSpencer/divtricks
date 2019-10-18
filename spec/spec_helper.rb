59f2908a569fde11790ce05e86779ad65908696a
Ken Spencer <me@iotaspencer.me>
2019-10-18 18:30:06 -0400

    make tests/specs

---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require 'rspec'
+RSpec.configure do |config|
+    config.expect_with :rspec do |expectations|
+      expectations.syntax = :expect
+    end
+  end
---
+++ spec/specs/divtricks_spec.rb
@@ -0,0 +1,7 @@
+require_relative '../spec_helper.rb'
+
+RSpec.describe 'String#div2' do
+  it "has a balance of zero when first created" do
+    expect("16161".div3).to eq(true)
+  end
+end
